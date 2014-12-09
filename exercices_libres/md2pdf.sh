20d7d4b319e14784403b95229173769af929d0be
nico <nicolas.jeanmonod@gmail.com>
2014-12-09 13:17:26 +0100

    update

--- exercices_libres/md2pdf.sh
+++ exercices_libres/md2pdf.sh
@@ -2,9 +2,17 @@
 
 MDFILE="exercices_libres.md"
 HTMLFILE="exercices_libres.html"
+tempHTMLFILE="/tmp/$HTMLFILE"
 PDFILE="exercices_libres.pdf"
 
-multimarkdown $MDFILE --smart  --full -o $HTMLFILE
+
+multimarkdown $MDFILE   --full -o $HTMLFILE
+sed 's/&#8230;/.../g' $HTMLFILE > $tempHTMLFILE && mv $tempHTMLFILE $HTMLFILE
+
+# Carctère en police Symbol
+#sed 's/⋅/./g' $HTMLFILE > $tempHTMLFILE && mv $tempHTMLFILE $HTMLFILE
+#sed 's/⇒/->/g' $HTMLFILE > $tempHTMLFILE && mv $tempHTMLFILE $HTMLFILE
+
 prince $HTMLFILE -s style_print.css -o $PDFILE --verbose --input=HTML
 open $PDFILE
 
